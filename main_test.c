e5f29d6d93232cd1d521365c7390200c2e862944
young_vd <yanfedoseev@gmail.com>
2019-08-18 18:07:33 +0300

    day14

--- main_test.c
+++ main_test.c
@@ -7,13 +7,27 @@ int main()
 {
     puts("test1");
     puts("printf");
-    printf("%hd", 32767);
+    printf("%d", 1);
     puts("");
     puts("ft_printf");
-    ft_printf("%hd", 32767);
+    ft_printf("%d", 1);
     puts("");
     puts("");
-
+//   CHK_FT_PRINTF_LIST+=("d" "%4.15d|42")
+//   CHK_FT_PRINTF_LIST+=("d" "%.2d|4242")
+//   CHK_FT_PRINTF_LIST+=("d" "%.10d|4242")
+//   CHK_FT_PRINTF_LIST+=("d" "%10.5d|4242")
+//   CHK_FT_PRINTF_LIST+=("d" "%-10.5d|4242")
+//   CHK_FT_PRINTF_LIST+=("d" "% 10.5d|4242")
+//   CHK_FT_PRINTF_LIST+=("d" "%+10.5d|4242")
+//   CHK_FT_PRINTF_LIST+=("d" "%-+10.5d|4242")
+//   CHK_FT_PRINTF_LIST+=("d" "%03.2d|0")
+//   CHK_FT_PRINTF_LIST+=("d" "%03.2d|1")
+//   CHK_FT_PRINTF_LIST+=("d" "%03.2d|-1")
+//   CHK_FT_PRINTF_LIST+=("d" "@moulitest: %.10d|-42")
+//   CHK_FT_PRINTF_LIST+=("d" "@moulitest: %.d %.0d|42|43")
+//   CHK_FT_PRINTF_LIST+=("d" "@moulitest: %.d %.0d|0|0")
+//   CHK_FT_PRINTF_LIST+=("d" "@moulitest: %5.d %5.0d|0|0")
 
     return (0);
 }
