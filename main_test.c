d44fb7efe9bb9e6761bd4b29f7feb64d4ccb7195
Loris Georgine <lgeorgin@il-h5.21-school.ru>
2019-08-30 21:03:03 +0300

    day18_v2

--- main_test.c
+++ main_test.c
@@ -3,30 +3,17 @@
 
 int ft_printf(const char *str, ...);
 
-// int main()
-// {
-//     int i = 0;
-
-//     //i = printf("%s\n", NULL);
-//     //printf("\ni = %i\n", i);
-//     puts("test0");
-//     puts("printf");
-//     printf("%s", "abc");
-//     puts("");
-//     puts("ft_printf");
-//     ft_printf("%s", "abc");
-//     puts("");
-//     puts("");
-
-//     return (0);
-// }
-
-int				main(void)
+int main()
 {
-   ft_printf("%20.15d\n", 54321);
-//   ft_printf("%-10d\n", 3);
-//   ft_printf("% d\n", 3);
-//   ft_printf("%+d\n", 3);
-//   ft_printf("%010d\n", 1);
-  return (0);
+	int a = -42;
+	
+	printf("%llu\n", a);
+	ft_printf("%llu\n", a);
+	puts("");
+	
+	printf("%llu\n", (unsigned long long)a);
+	ft_printf("%llu\n", (unsigned long long)a);
+	puts("");
+
+	return (0);
 }
