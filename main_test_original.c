8e8fdc7328ea9dd27dabcfae2a14fd9fb77e1482
Loris Georgine <lgeorgin@at-r1.21-school.ru>
2020-02-09 00:05:12 +0300

    day50_v5

--- main_test_original.c
+++ main_test_original.c
@@ -10,223 +10,222 @@ int main()
     ft_printf("%s", "abc");
     puts("");
 
-    // printf("%+#o\n", -10);
-    // ft_printf("%+#o\n", -10);
+    printf("%+#o\n", -10);
+    ft_printf("%+#o\n", -10);
+    puts("");
+
+    printf("%+#o\n", -10);
+    ft_printf("%+#o\n", -10);
+    puts("");
+
+    printf("123%c%s%lc%s\n", 'a', "abc", 'a', "abc");
+    ft_printf("123%c%s%lc%s\n", 'a', "abc", 'a', "abc");
+
+    const double RENT = 3852.51;
+    char *str = "ada";
+
+    printf("%010d\n", 123123123123);
+    ft_printf("%010d\n", 123123123123);
+
+    printf("%*.*f\n", 10, 2 , 999.1551);
+    ft_printf("%*.*f\n", 10, 2, 999.1551);
+
+    printf("023%23.2s\n", " start", 10, 2 , 999.1551);
+    ft_printf("023%23.2s\n", " start", 10, 2, 999.1551);
+    //puts("");
+
+    printf("023%23.8s%*.*f\n", " start", 10, 0 , 999.1);
+    ft_printf("023%23.8s%*.*f\n", " start", 10, 0, 999.1);
+    puts("");
+
+    printf("%-10.3s\n", "  str");
+    ft_printf("%-10.3s\n", "  str");
+    puts("");
+
+    printf("%10.2p\n", NULL);
+    ft_printf("%10.2p\n", NULL);
+    puts("");
+
+    printf("%.1d\n", 123);
+    ft_printf("%.1d\n", 123);
+    puts("");
+
+    printf("%0+08.5i\n", -123);
+    ft_printf("%0+08.5i\n", -123);
+    printf("%0+08.5i\n", 123);
+    ft_printf("%0+08.5i\n", 123);
+    printf("%08.5i\n", 123);
+    ft_printf("%08.5i\n", 123);
+    printf("%08i\n", 123);
+    ft_printf("%08i\n", 123);
+
+    printf("%+08i\n", 123);
+    ft_printf("%+08i\n", 123);
+    printf("%08i\n", -123);
+    ft_printf("%08i\n", -123);
+    printf("%+ 8i\n", 123);
+    ft_printf("%+ 8i\n", 123);
+    printf("% 8i\n", -123);
+    ft_printf("% 8i\n", -123);
+    puts("");
+
+    printf("%i\n", atoi("+00123"));
+    ft_printf("%i\n", atoi("+00123"));
+
+    //ft_printf("%+10i\n", 10);
+    printf("%+08.5i\n", 123);
+    ft_printf("%+08.5i\n", 123);
+    //printf("%.4f\n", 10.12345);
+    //ft_printf("%.4f\n", 10.1);
+    puts("");
+
+    // printf("%010c\n", 'c');
+    // printf("%010d\n", 10);
+    // printf("%010i\n", 10);
+    // printf("%010f\n", 10.1);
+    // printf("%010o\n", 10);
+    // printf("%010p\n", (void *) 10);
+    // printf("%010s\n", "str");
+    // printf("%010u\n", 10);
+    // printf("%010x\n", 10);
+    // printf("%010X\n", 10);
     // puts("");
 
-    // printf("%+#o\n", -10);
-    // ft_printf("%+#o\n", -10);
-    // puts("");
+    printf("%o\n", 10);
+    ft_printf("%o\n", 10);
+    puts("");
 
-    // printf("123%c%s%lc%s\n", 'a', "abc", 'a', "abc");
-    // ft_printf("123%c%s%lc%s\n", 'a', "abc", 'a', "abc");
+    printf("%#o\n", 10);
+    ft_printf("%#o\n", 10);
+    puts("");
 
-    // const double RENT = 3852.51;
-    // char *str = "ada";
+    printf("%o\n", -10);
+    ft_printf("%o\n", -10);
+    puts("");
 
-    // printf("%010d\n", 123123123123);
-    // ft_printf("%010d\n", 123123123123);
+    printf("%#o\n", -10);
+    ft_printf("%#o\n", -10);
+    puts("");
 
-    // printf("%*.*f\n", 10, 2 , 999.1551);
-    // ft_printf("%*.*f\n", 10, 2, 999.1551);
+    printf("%x\n", 10);
+    ft_printf("%x\n", 10);
+    puts("");
 
-    // printf("023%23.2s\n", " start", 10, 2 , 999.1551);
-    // ft_printf("023%23.2s\n", " start", 10, 2, 999.1551);
-    // //puts("");
+    printf("%#x\n", 10);
+    ft_printf("%#x\n", 10);
+    puts("");
 
-    // printf("023%23.8s%*.*f\n", " start", 10, 0 , 999.1);
-    // ft_printf("023%23.8s%*.*f\n", " start", 10, 0, 999.1);
-    // puts("");
+    printf("%x\n", -10);
+    ft_printf("%x\n", -10);
+    puts("");
 
-    // printf("%-10.3s\n", "  str");
-    // ft_printf("%-10.3s\n", "  str");
-    // puts("");
+    printf("%#x\n", -10);
+    ft_printf("%#x\n", -10);
+    puts("");
 
-    // printf("%10.2p\n", NULL);
-    // ft_printf("%10.2p\n", NULL);
-    // puts("");
+    printf("%+x\n", 10);
+    ft_printf("%+x\n", 10);
+    puts("");
 
-    // printf("%.1d\n", 123);
-    // ft_printf("%.1d\n", 123);
-    // puts("");
+    printf("%+#x\n", 10);
+    ft_printf("%+#x\n", 10);
+    puts("");
 
-    // printf("%0+08.5i\n", -123);
-    // ft_printf("%0+08.5i\n", -123);
-    // printf("%0+08.5i\n", 123);
-    // ft_printf("%0+08.5i\n", 123);
-    // printf("%08.5i\n", 123);
-    // ft_printf("%08.5i\n", 123);
-    // printf("%08i\n", 123);
-    // ft_printf("%08i\n", 123);
+    printf("%+x\n", -10);
+    ft_printf("%+x\n", -10);
+    puts("");
 
-    // printf("%+08i\n", 123);
-    // ft_printf("%+08i\n", 123);
-    // printf("%08i\n", -123);
-    // ft_printf("%08i\n", -123);
-    // printf("%+ 8i\n", 123);
-    // ft_printf("%+ 8i\n", 123);
-    // printf("% 8i\n", -123);
-    // ft_printf("% 8i\n", -123);
-    // puts("");
+    printf("%+#x\n", -10);
+    ft_printf("%+#x\n", -10);
+    puts("");
 
-    // printf("%i\n", atoi("+00123"));
-    // ft_printf("%i\n", atoi("+00123"));
+    printf("%+o\n", 10);
+    ft_printf("%+o\n", 10);
+    puts("");
 
-    // //ft_printf("%+10i\n", 10);
-    // printf("%+08.5i\n", 123);
-    // ft_printf("%+08.5i\n", 123);
-    // //printf("%.4f\n", 10.12345);
-    // //ft_printf("%.4f\n", 10.1);
-    // puts("");
+    printf("%+#o\n", 10);
+    ft_printf("%+#o\n", 10);
+    puts("");
 
-    // // printf("%010c\n", 'c');
-    // // printf("%010d\n", 10);
-    // // printf("%010i\n", 10);
-    // // printf("%010f\n", 10.1);
-    // // printf("%010o\n", 10);
-    // // printf("%010p\n", (void *) 10);
-    // // printf("%010s\n", "str");
-    // // printf("%010u\n", 10);
-    // // printf("%010x\n", 10);
-    // // printf("%010X\n", 10);
-    // // puts("");
+    printf("%+o\n", -10);
+    ft_printf("%+o\n", -10);
+    puts("");
 
-    // printf("%o\n", 10);
-    // ft_printf("%o\n", 10);
-    // puts("");
+    int n = printf("1%23.0f\n", 12.3);
+    int m = ft_printf("1%23.0f\n", 12.3);
+    printf("%d = %d\n", n, m);
+    puts("");
 
-    // printf("%#o\n", 10);
-    // ft_printf("%#o\n", 10);
-    // puts("");
+    printf("123%010%%c%s\n", 'a', "abc");
+    ft_printf("123%010%%c%s\n", 'a', "abc");
+    puts("");
 
-    // printf("%o\n", -10);
-    // ft_printf("%o\n", -10);
-    // puts("");
+    printf("123%10.10%%c%s\n", 'a', "abc");
+    ft_printf("123%10.10%%c%s\n", 'a', "abc");
+    puts("");
 
-    // printf("%#o\n", -10);
-    // ft_printf("%#o\n", -10);
-    // puts("");
-
-    // printf("%x\n", 10);
-    // ft_printf("%x\n", 10);
-    // puts("");
-
-    // printf("%#x\n", 10);
-    // ft_printf("%#x\n", 10);
-    // puts("");
-
-    // printf("%x\n", -10);
-    // ft_printf("%x\n", -10);
-    // puts("");
-
-    // printf("%#x\n", -10);
-    // ft_printf("%#x\n", -10);
-    // puts("");
-
-    // printf("%+x\n", 10);
-    // ft_printf("%+x\n", 10);
-    // puts("");
-
-    // printf("%+#x\n", 10);
-    // ft_printf("%+#x\n", 10);
-    // puts("");
-
-    // printf("%+x\n", -10);
-    // ft_printf("%+x\n", -10);
-    // puts("");
-
-    // printf("%+#x\n", -10);
-    // ft_printf("%+#x\n", -10);
-    // puts("");
-
-    // printf("%+o\n", 10);
-    // ft_printf("%+o\n", 10);
-    // puts("");
-
-    // printf("%+#o\n", 10);
-    // ft_printf("%+#o\n", 10);
-    // puts("");
-
-    // printf("%+o\n", -10);
-    // ft_printf("%+o\n", -10);
-    // puts("");
-
-    // int n = printf("1%23.0f\n", 12.3);
-    // int m = ft_printf("1%23.0f\n", 12.3);
-    // printf("%d = %d\n", n, m);
-    // puts("");
-
-    // printf("123%010%%c%s\n", 'a', "abc");
-    // ft_printf("123%010%%c%s\n", 'a', "abc");
-    // puts("");
-
-    // printf("123%10.10%%c%s\n", 'a', "abc");
-    // ft_printf("123%10.10%%c%s\n", 'a', "abc");
-    // puts("");
-
-    // printf("%x %X %#x\n", 1234234, 1234234, 1234234);
-    // ft_printf("%x %X %#x\n", 1234234, 1234234, 1234234);
-    // puts("");
+    printf("%x %X %#x\n", 1234234, 1234234, 1234234);
+    ft_printf("%x %X %#x\n", 1234234, 1234234, 1234234);
+    puts("");
 
 
-    // printf("%p %p* %5p %-20p\n", "a", "", "", "");
-    // ft_printf("%p %p* %5p %-20p\n", "a", "", "", "");
-    // puts("");
+    printf("%p %p* %5p %-20p\n", "a", "", "", "");
+    ft_printf("%p %p* %5p %-20p\n", "a", "", "", "");
+    puts("");
 
-    // printf("**%d**% d% d **\n", 42, 42, -42);
-    // ft_printf("**%d**% d% d **\n", 42, 42, -42);
-    // puts("");
+    printf("**%d**% d% d **\n", 42, 42, -42);
+    ft_printf("**%d**% d% d **\n", 42, 42, -42);
+    puts("");
 
-    // printf("%#o\n", 10);
-    // ft_printf("%#o\n", 10);
-    // puts("");
+    printf("%#o\n", 10);
+    ft_printf("%#o\n", 10);
+    puts("");
 
-    // printf("%u\n", 10);
-    // ft_printf("%u\n", 10);
-    // puts("");
+    printf("%u\n", 10);
+    ft_printf("%u\n", 10);
+    puts("");
 
-    // printf("*%f*\n", RENT);
-    // ft_printf("*%f*\n", RENT);
-	// puts("");
+    printf("*%f*\n", RENT);
+    ft_printf("*%f*\n", RENT);
+	puts("");
 
-    // printf("*%4.0f*\n", RENT);
-    // ft_printf("*%4.0f*\n", RENT);
-	// puts("");
+    printf("*%4.0f*\n", RENT);
+    ft_printf("*%4.0f*\n", RENT);
+	puts("");
 
-    // printf("123%L%%c%s\n", 'a', "abc");
-    // ft_printf("123%L%%c%s\n", 'a', "abc");
-    // printf("123%ll%%c%s\n", 'a', "abc");
-    // ft_printf("123%ll%%c%s\n", 'a', "abc");
-    // printf("123%l%%c%s\n", 'a', "abc");
-    // ft_printf("123%l%%c%s\n", 'a', "abc");
-    // printf("123%hh%%c%s\n", 'a', "abc");
-    // ft_printf("123%hh%%c%s\n", 'a', "abc");
-    // printf("123%h%%c%s\n", 'a', "abc");
-    // ft_printf("123%h%%c%s\n", 'a', "abc");
-    // //printf("123%hmmm%hmmm%hmmm%lmmm%Lmmm\n", 'a', "abc");
-    // //ft_printf("123%hmmm%hmmm%hmmm%lmmm%Lmmm\n", 'a', "abc");
-    // //в строке ниже добавлена 'm' и начиная с нее ft_printf ничего не выводит
-    // //standard printf also drops a warning. Not sure what bahaviour do you expect to see here
-    // printf("123%c%ms%lc%s\n", 'a', "abc", 'a', "abc");
-    // ft_printf("123%c%ms%lc%s\n", 'a', "abc", 'a', "abc");
-    // puts("");
+    printf("123%L%%c%s\n", 'a', "abc");
+    ft_printf("123%L%%c%s\n", 'a', "abc");
+    printf("123%ll%%c%s\n", 'a', "abc");
+    ft_printf("123%ll%%c%s\n", 'a', "abc");
+    printf("123%l%%c%s\n", 'a', "abc");
+    ft_printf("123%l%%c%s\n", 'a', "abc");
+    printf("123%hh%%c%s\n", 'a', "abc");
+    ft_printf("123%hh%%c%s\n", 'a', "abc");
+    printf("123%h%%c%s\n", 'a', "abc");
+    ft_printf("123%h%%c%s\n", 'a', "abc");
+    //printf("123%hmmm%hmmm%hmmm%lmmm%Lmmm\n", 'a', "abc");
+    //ft_printf("123%hmmm%hmmm%hmmm%lmmm%Lmmm\n", 'a', "abc");
+    //в строке ниже добавлена 'm' и начиная с нее ft_printf ничего не выводит
+    //standard printf also drops a warning. Not sure what bahaviour do you expect to see here
+    printf("123%c%ms%lc%s\n", 'a', "abc", 'a', "abc");
+    ft_printf("123%c%ms%lc%s\n", 'a', "abc", 'a', "abc");
+    puts("");
 
-    // printf("123%*.*f\n", 10,2 ,999);
-    // printf("\nOK?\n");
-    // ft_printf("123%*.*f\n", 10, 2, 999);
-    // puts("");
+    printf("123%*.*f\n", 10,2 ,999);
+    printf("\nOK?\n");
+    ft_printf("123%*.*f\n", 10, 2, 999);
+    puts("");
 
-    // printf("123%10d\n", 999);
-    // printf("\nOK?\n");
-    // ft_printf("123%10d\n", 999);
-    // puts("");
+    printf("123%10d\n", 999);
+    printf("\nOK?\n");
+    ft_printf("123%10d\n", 999);
+    puts("");
 
-    // printf("123%.2f\n", 999);
-    // printf("\nOK?\n");
-    // ft_printf("123%.2f\n", 999);
-    // puts("");
+    printf("123%.2f\n", 999);
+    printf("\nOK?\n");
+    ft_printf("123%.2f\n", 999);
+    puts("");
 
-	/*
     //printf("123%.10-+ 0#10.10d\n", 777);
 	printf("123%.12+ 0#15.8d\n", 777);
     ft_printf("123%.12+ 0#15.8d\n", 777);
@@ -279,7 +278,6 @@ int main()
     printf("%X %X\n", b, d);
     printf("%X %X\n", (unsigned long int)b, (unsigned long long int)d);
     printf("%lX %llX\n", b, d);
-	*/
 
     return (0);
 }
